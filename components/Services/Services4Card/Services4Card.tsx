e76e37c978732c055ad2c08fc94cfa89c9c37229
clay <clayborne.harrison@sykescottages.co.uk>
2023-09-13 19:27:43 +0100

    fixing services4card

--- components/Services/Services4Card/Services4Card.tsx
+++ components/Services/Services4Card/Services4Card.tsx
@@ -1,37 +1,78 @@
 import React from 'react';
-import styles from './Services4Card.module.css';
-import { services } from './services';
-import Image from 'next/image';
-import Link from 'next/link';
+import styles from './Services4Card.module.css'; // Import your CSS module
 
 const Services4Card = () => {
-    return (
-        <section className={styles.services4Card}>
-            <div className={`w-full m-auto flex flex-col items-center ${styles.csContainer}`}>
-                <div className={`text-center w-full flex flex-col items-center`}>
-                    <span className={`uppercase font-bold block mb-1 ${styles.csTopper}`}>Our Services</span>
-                    <h2 className={`font-black mb-4 relative ${styles.csTitle}`}>We Are Committed To Delivering Top Quality Legal Services</h2>
-                    <p className={`w-full ${styles.csText}`}>
-                        At Stitch Law, let us help you solve problems so that you can focus on your mission. We support businesses through periods of expansion, succession.
-                    </p>
-                </div>
-                <ul className={`w-full flex flex-col justify-center items-center md:justify-between md:align-stretch md:flex-row md:flex-wrap xl:justify-center ${styles.csCardGroup}`}>
-                    {services.map((service) => (
-                        <li key={service.id} className={`text-left w-full p-8 overflow-hidden relative rounded-2xl hover:cursor-pointer md:w-1/2 ${styles.csItem}`}>
-                            <Link href="" className={`${styles.csLink}`}>
-                                <service.icon styling={`w-auto mb-6 block ${styles.csIcon}`} />
-                                <h3 className={`mb-4 font-bold ${styles.csH3}`}>{service.title}</h3>
-                                <p className={`text-base ${styles.csItemText}`}>{service.description}</p>
-                            </Link>
-                            <picture className={`h-full w-full absolute opacity-0 top-0 left-0 block ${styles.csImage}`} aria-hidden="true">
-                                <Image src={service.image} alt='library' />
-                            </picture>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    );
+  return (
+    <section className={styles.services4Card}>
+      <div className={styles.csContainer}>
+        <div className={styles.csContent}>
+          <span className={styles.csTopper}>Our Services</span>
+          <h2 className={styles.csTitle}>We Are Committed To Delivering Top Quality Legal Services</h2>
+          <p className={styles.csText}>
+            At Stitch Law, let us help you solve problems so that you can focus on your mission. We support businesses through periods of expansion, succession.
+          </p>
+        </div>
+        <ul className={styles.csCardGroup}>
+          <li className={styles.csItem}>
+            <a href="" className={styles.csLink}>
+              <img className={styles.csIcon} src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons%2Fcase.svg" alt="icon" width="" height="" loading="lazy" decoding="async" aria-hidden="true" />
+              <h3 className={styles.csH3}>99% winning guarantee</h3>
+              <p className={styles.csItemText}>
+                Full range of shipping disputes such as charter party disputes, demurrage claims, purchase agreements, trade finance, carriage and claims.
+              </p>
+            </a>
+            <picture className={styles.csImage} aria-hidden="true">
+              <source media="(max-width: 600px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <source media="(min-width: 601px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" alt="library" width="413" height="266" />
+            </picture>
+          </li>
+          <li className={styles.csItem}>
+            <a href="" className={styles.csLink}>
+              <img className={styles.csIcon} src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons%2Fcase.svg" alt="icon" width="" height="" loading="lazy" decoding="async" aria-hidden="true" />
+              <h3 className={styles.csH3}>Secure management</h3>
+              <p className={styles.csItemText}>
+                Full range of shipping disputes such as charter party disputes, demurrage claims, purchase agreements, trade finance, carriage and claims.
+              </p>
+            </a>
+            <picture className={styles.csImage} aria-hidden="true">
+              <source media="(max-width: 600px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <source media="(min-width: 601px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" alt="library" width="413" height="266" />
+            </picture>
+          </li>
+          <li className={styles.csItem}>
+            <a href="" className={styles.csLink}>
+              <img className={styles.csIcon} src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons%2Fcase.svg" alt="icon" width="" height="" loading="lazy" decoding="async" aria-hidden="true" />
+              <h3 className={styles.csH3}>Full time support</h3>
+              <p className={styles.csItemText}>
+                Full range of shipping disputes such as charter party disputes, demurrage claims, purchase agreements, trade finance, carriage and claims.
+              </p>
+            </a>
+            <picture className={styles.csImage} aria-hidden="true">
+              <source media="(max-width: 600px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <source media="(min-width: 601px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" alt="library" width="413" height="266" />
+            </picture>
+          </li>
+          <li className={styles.csItem}>
+            <a href="" className={styles.csLink}>
+              <img className={styles.csIcon} src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons%2Fcase.svg" alt="icon" width="" height="" loading="lazy" decoding="async" aria-hidden="true" />
+              <h3 className={styles.csH3}>Family Law</h3>
+              <p className={styles.csItemText}>
+                We’ve helped thousands of organizations transform to become relevant of helped thousands.
+              </p>
+            </a>
+            <picture className={styles.csImage} aria-hidden="true">
+              <source media="(max-width: 600px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <source media="(min-width: 601px)" srcSet="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" />
+              <img loading="lazy" decoding="async" src="https://csimg.nyc3.digitaloceanspaces.com/Services/library.jpg" alt="library" width="413" height="266" aria-hidden="true" />
+            </picture>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Services4Card;
