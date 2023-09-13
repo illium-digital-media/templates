b8b8e25cd2ce6f3ce3302bfa179a35c771ef1050
clay <clayborne.harrison@sykescottages.co.uk>
2023-09-13 17:37:22 +0100

    added tailwind to services

--- components/Services/Services4Card/Services4Card.tsx
+++ components/Services/Services4Card/Services4Card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Services4Card.module.css';
 import { services } from './services';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Services4Card = () => {
     return (
@@ -10,21 +11,19 @@ const Services4Card = () => {
                 <div className={`text-center w-full flex flex-col items-center`}>
                     <span className={`uppercase font-bold block mb-1 ${styles.csTopper}`}>Our Services</span>
                     <h2 className={`font-black mb-4 relative ${styles.csTitle}`}>We Are Committed To Delivering Top Quality Legal Services</h2>
-                    <p className={`w-full m-0 ${styles.csText}`}>
+                    <p className={`w-full ${styles.csText}`}>
                         At Stitch Law, let us help you solve problems so that you can focus on your mission. We support businesses through periods of expansion, succession.
                     </p>
                 </div>
-                <ul className={`w-full m-0 p-0 flex flex-col justify-center items-center ${styles.csCardGroup}`}>
+                <ul className={`w-full flex flex-col justify-center items-center md:justify-between md:align-stretch md:flex-row md:flex-wrap xl:justify-center ${styles.csCardGroup}`}>
                     {services.map((service) => (
-                        <li key={service.id} className={`text-left w-full m-0 p-8 overflow-hidden relative rounded-2xl hover:cursor-pointer hover:opacity-100 ${styles.csItem}`}>
-                            <a href="" className={`no-underline ${styles.csLink}`}>
-                                <service.icon styling={styles.csIcon} />
-                                <h3 className={styles.csH3}>{service.title}</h3>
-                                <p className={styles.csItemText}>{service.description}</p>
-                            </a>
+                        <li key={service.id} className={`text-left w-full p-8 overflow-hidden relative rounded-2xl hover:cursor-pointer md:w-1/2 ${styles.csItem}`}>
+                            <Link href="" className={`${styles.csLink}`}>
+                                <service.icon styling={`w-auto mb-6 block ${styles.csIcon}`} />
+                                <h3 className={`mb-4 font-bold ${styles.csH3}`}>{service.title}</h3>
+                                <p className={`text-base ${styles.csItemText}`}>{service.description}</p>
+                            </Link>
                             <picture className={`h-full w-full absolute opacity-0 top-0 left-0 block ${styles.csImage}`} aria-hidden="true">
-                                <source media="(max-width: 600px)" srcSet={service.image.src} />
-                                <source media="(min-width: 601px)" srcSet={service.image.src} />
                                 <Image src={service.image} alt='library' />
                             </picture>
                         </li>
